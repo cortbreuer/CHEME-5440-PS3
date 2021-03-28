760548ff3fe3960b4f1fc5a5d009c8412f22dc46
Cort Breuer <cb732@cornell.edu>
2021-03-28 15:43:40 -0400

    Notation

--- src/Problem.jl
+++ src/Problem.jl
@@ -50,28 +50,20 @@ function generate_problem_dictionary()
 	# TODO: each row is a reaction
 	E = .01 # umol/gDW
 
-	flux_bounds_array[1,:] .= [0, 10]
-	flux_bounds_array[2,:] .= [0, 10]
-	flux_bounds_array[3,:] .= [0, 10]
-	flux_bounds_array[4,:] .= [0, 10]
+	flux_bounds_array[1,:] = [0, 10]
+	flux_bounds_array[2,:] = [0, 10]
+	flux_bounds_array[3,:] = [0, 10]
+	flux_bounds_array[4,:] = [0, 10]
 
-	#flux_bounds_array[5:15,1] .= -10
-	#flux_bounds_array[5:15,2] .= 10
+	flux_bounds_array[5:15,1] .= -10
+	flux_bounds_array[5:15,2] .= 10
 
-	flux_bounds_array[5:19,1] .= -10
-	flux_bounds_array[5:19,2] .= 10
-
-	"""flux_bounds_array[16,:] = [0, E*203]
+	flux_bounds_array[16,:] = [0, E*203]
 	flux_bounds_array[17,:] = [0, E*34.5]
 	flux_bounds_array[18,:] = [0, E*249]
 	flux_bounds_array[19,:] = [0, E*88.1]
-	flux_bounds_array[20,:] = [-E*13.7, E*13.7]"""
+	flux_bounds_array[20,:] = [-E*13.7, E*13.7]
 
-	flux_bounds_array[20,:] = [0, E*203]
-	flux_bounds_array[21,:] = [0, E*34.5]
-	flux_bounds_array[22,:] = [0, E*249]
-	flux_bounds_array[23,:] = [0, E*88.1]
-	flux_bounds_array[24,:] = [-E*13.7, E*13.7]
 	#@show(flux_bounds_array)
 
 	# Setup default species bounds array -
